562c0b369a28b44954c5b4d611d1ffdec36f662a
Pavinthan <pavinthan@outlook.com>
2019-06-01 00:13:02 +0530

    simplify react component

--- src/Presets/react-stubs/Example.js
+++ src/Presets/react-stubs/Example.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Example extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
 
-                            <div className="card-body">I'm an example component!</div>
-                        </div>
+                        <div className="card-body">I'm an example component!</div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 if (document.getElementById('example')) {
