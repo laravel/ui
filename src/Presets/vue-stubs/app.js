ec059a277d63f3b13aa9260ddaa29b002071b16d
Jared Lewis <jslewis90@outlook.com>
2019-08-21 16:00:49 -0400

    Updated require to import

--- src/Presets/vue-stubs/app.js
+++ src/Presets/vue-stubs/app.js
@@ -6,7 +6,8 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+window.Vue = Vue;
 
 /**
  * The following block of code may be used to automatically register your
