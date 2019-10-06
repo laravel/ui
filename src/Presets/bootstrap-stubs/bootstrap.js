a2614049fcc70a38acd33abc2e080455193b3236
Roberto Aguilar <roberto.aguilar.arrieta@gmail.com>
2019-10-06 14:41:53 -0500

    Add missing semicolon to avoid unnecesary diff
    The default laravel's bootstrap.js file has a semicolon in the Echo
    import statement and even though the missing one in the scaffolded file
    is not strictly required, it causes an unnecessary diff if it is not
    there when the scaffolding happens.

--- src/Presets/bootstrap-stubs/bootstrap.js
+++ src/Presets/bootstrap-stubs/bootstrap.js
@@ -29,7 +29,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+// import Echo from 'laravel-echo';
 
 // window.Pusher = require('pusher-js');
 
