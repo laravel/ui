89009d8c756f00c668b4a8a63a9705c025294b27
4nh51rk <4nh51rk@users.noreply.github.com>
2021-11-16 12:03:52 +0530

    Changed 'verification.resend' Route to GET
    $this->post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
    Causes a Method Not Allowed Http Exception

--- src/AuthRouteMethods.php
+++ src/AuthRouteMethods.php
@@ -90,7 +90,7 @@ class AuthRouteMethods
         return function () {
             $this->get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
             $this->get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
-            $this->post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+            $this->get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
         };
     }
 }
