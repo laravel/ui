6846510e1e5793ac6f0f06745e69e0833bcc62fd
Taylor Otwell <taylor@laravel.com>
2021-11-30 10:06:40 -0600

    Update bootstrap.js

--- src/Presets/bootstrap-stubs/bootstrap.js
+++ src/Presets/bootstrap-stubs/bootstrap.js
@@ -1,11 +1,5 @@
 window._ = require('lodash');
 
-/**
- * We'll load Bootstrap library which provides support for JavaScript based
- * Bootstrap features such as modals and tabs. This code may be modified to
- * fit the specific needs of your application.
- */
-
 try {
     require('bootstrap');
 } catch (e) {}
