b0ee8ca95496a49464905572cea9da5cbf5bd104
Sébastien QUIAIOS <13620819+squiaios@users.noreply.github.com>
2022-09-16 14:09:13 +0200

    feat: move functions from test to controller

--- auth-backend/RegistersUsers.php
+++ auth-backend/RegistersUsers.php
@@ -65,4 +65,38 @@ trait RegistersUsers
     {
         //
     }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+     /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        $user = (new User())->forceFill([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $user->save();
+
+        return $user;
+    }
 }
