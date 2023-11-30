7c571004fc50fdd0fded950b8233c4ea583e2476
Nuno Maduro <enunomaduro@gmail.com>
2023-11-30 12:42:25 +0000

    Overrides service provider if needed

--- src/AuthCommand.php
+++ src/AuthCommand.php
@@ -117,20 +117,33 @@ class AuthCommand extends Command
 
         if (file_exists($controller) && ! $this->option('force')) {
             if ($this->components->confirm("The [HomeController.php] file already exists. Do you want to replace it?")) {
-                file_put_contents($controller, $this->compileControllerStub('HomeController'));
+                file_put_contents($controller, $this->compileStub('controllers/HomeController'));
             }
         } else {
-            file_put_contents($controller, $this->compileControllerStub('HomeController'));
+            file_put_contents($controller, $this->compileStub('controllers/HomeController'));
         }
 
         $baseController = app_path('Http/Controllers/Controller.php');
 
         if (file_exists($baseController) && ! $this->option('force')) {
             if ($this->components->confirm("The [Controller.php] file already exists. Do you want to replace it?")) {
-                file_put_contents($baseController, $this->compileControllerStub('Controller'));
+                file_put_contents($baseController, $this->compileStub('controllers/Controller'));
             }
         } else {
-            file_put_contents($baseController, $this->compileControllerStub('Controller'));
+            file_put_contents($baseController, $this->compileStub('controllers/Controller'));
+        }
+
+        if (class_exists('Illuminate\Auth\Middleware\RedirectIfAuthenticated')
+            && method_exists('Illuminate\Auth\Middleware\RedirectIfAuthenticated', 'redirectUsing')) {
+            $appProvider = app_path('Providers/AppServiceProvider.php');
+
+            if (file_exists($appProvider) && ! $this->option('force')) {
+                if ($this->components->confirm("The [AppServiceProvider.php] file already exists. Do you want to replace it?")) {
+                    file_put_contents($appProvider, $this->compileStub('providers/AppServiceProvider'));
+                }
+            } else {
+                file_put_contents($appProvider, $this->compileStub('providers/AppServiceProvider'));
+            }
         }
 
         file_put_contents(
@@ -151,12 +164,12 @@ class AuthCommand extends Command
      * @param  string  $stub
      * @return string
      */
-    protected function compileControllerStub($stub)
+    protected function compileStub($stub)
     {
         return str_replace(
             '{{namespace}}',
             $this->laravel->getNamespace(),
-            file_get_contents(__DIR__.'/Auth/stubs/controllers/'.$stub.'.stub')
+            file_get_contents(__DIR__.'/Auth/stubs/'.$stub.'.stub')
         );
     }
 
