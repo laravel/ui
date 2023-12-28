a89534c26ce4cbb33979b5ad61ccfe55b4ad864e
Nuno Maduro <enunomaduro@gmail.com>
2023-12-28 16:48:12 +0000

    [develop] Adds L11 support (#257)
    * Adds L11 support
    
    * Fixes missing readme
    
    * Removes references to `RouteServiceProvider`
    
    * Injects `Controller` stub
    
    * Adds missing `.php` suffix
    
    * Fixes stubs
    
    * Overrides service provider if needed
    
    * Fixes namespace
    
    * Redirects home
    
    * docs
    
    * Installs `2014_10_12_100000_create_password_resets_table` for older Laravel versions
    
    * Only copies reset passports before Laravel 11

--- src/AuthCommand.php
+++ src/AuthCommand.php
@@ -117,10 +117,27 @@ class AuthCommand extends Command
 
         if (file_exists($controller) && ! $this->option('force')) {
             if ($this->components->confirm("The [HomeController.php] file already exists. Do you want to replace it?")) {
-                file_put_contents($controller, $this->compileControllerStub());
+                file_put_contents($controller, $this->compileStub('controllers/HomeController'));
             }
         } else {
-            file_put_contents($controller, $this->compileControllerStub());
+            file_put_contents($controller, $this->compileStub('controllers/HomeController'));
+        }
+
+        $baseController = app_path('Http/Controllers/Controller.php');
+
+        if (file_exists($baseController) && ! $this->option('force')) {
+            if ($this->components->confirm("The [Controller.php] file already exists. Do you want to replace it?")) {
+                file_put_contents($baseController, $this->compileStub('controllers/Controller'));
+            }
+        } else {
+            file_put_contents($baseController, $this->compileStub('controllers/Controller'));
+        }
+
+        if (! file_exists(database_path('migrations/0001_01_01_000000_create_users_table.php'))) {
+            copy(
+                __DIR__.'/../stubs/migrations/2014_10_12_100000_create_password_resets_table.php',
+                base_path('database/migrations/2014_10_12_100000_create_password_resets_table.php')
+            );
         }
 
         file_put_contents(
@@ -128,24 +145,20 @@ class AuthCommand extends Command
             file_get_contents(__DIR__.'/Auth/stubs/routes.stub'),
             FILE_APPEND
         );
-
-        copy(
-            __DIR__.'/../stubs/migrations/2014_10_12_100000_create_password_resets_table.php',
-            base_path('database/migrations/2014_10_12_100000_create_password_resets_table.php')
-        );
     }
 
     /**
-     * Compiles the "HomeController" stub.
+     * Compiles the given stub.
      *
+     * @param  string  $stub
      * @return string
      */
-    protected function compileControllerStub()
+    protected function compileStub($stub)
     {
         return str_replace(
             '{{namespace}}',
             $this->laravel->getNamespace(),
-            file_get_contents(__DIR__.'/Auth/stubs/controllers/HomeController.stub')
+            file_get_contents(__DIR__.'/Auth/stubs/'.$stub.'.stub')
         );
     }
 
