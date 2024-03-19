07c7b06204b57a5171721daf5caa5ea4c01b1400
Tim MacDonald <hello@timacdonald.me>
2024-03-19 12:20:39 +1100

    Make commands lazy

--- src/AuthCommand.php
+++ src/AuthCommand.php
@@ -4,7 +4,9 @@ namespace Laravel\Ui;
 
 use Illuminate\Console\Command;
 use InvalidArgumentException;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'ui:auth')]
 class AuthCommand extends Command
 {
     /**
