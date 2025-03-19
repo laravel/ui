3e2b5a7b769a661074ed31848dc354f3168a3b47
Mordi Sacks <13218192+MordiSacks@users.noreply.github.com>
2025-03-19 13:27:38 +0200

    added guard function to VerifiesEmails

--- auth-backend/VerifiesEmails.php
+++ auth-backend/VerifiesEmails.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 trait VerifiesEmails
 {
@@ -19,7 +20,7 @@ trait VerifiesEmails
      */
     public function show(Request $request)
     {
-        return $request->user()->hasVerifiedEmail()
+        return $this->guard()->user()->hasVerifiedEmail()
                         ? redirect($this->redirectPath())
                         : view('auth.verify');
     }
@@ -34,22 +35,22 @@ trait VerifiesEmails
      */
     public function verify(Request $request)
     {
-        if (! hash_equals((string) $request->route('id'), (string) $request->user()->getKey())) {
+        if (! hash_equals((string) $request->route('id'), (string) $this->guard()->user()->getKey())) {
             throw new AuthorizationException;
         }
 
-        if (! hash_equals((string) $request->route('hash'), sha1($request->user()->getEmailForVerification()))) {
+        if (! hash_equals((string) $request->route('hash'), sha1($this->guard()->user()->getEmailForVerification()))) {
             throw new AuthorizationException;
         }
 
-        if ($request->user()->hasVerifiedEmail()) {
+        if ($this->guard()->user()->hasVerifiedEmail()) {
             return $request->wantsJson()
                         ? new JsonResponse([], 204)
                         : redirect($this->redirectPath());
         }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
+        if ($this->guard()->user()->markEmailAsVerified()) {
+            event(new Verified($this->guard()->user()));
         }
 
         if ($response = $this->verified($request)) {
@@ -80,16 +81,26 @@ trait VerifiesEmails
      */
     public function resend(Request $request)
     {
-        if ($request->user()->hasVerifiedEmail()) {
+        if ($this->guard()->user()->hasVerifiedEmail()) {
             return $request->wantsJson()
                         ? new JsonResponse([], 204)
                         : redirect($this->redirectPath());
         }
 
-        $request->user()->sendEmailVerificationNotification();
+        $this->guard()->user()->sendEmailVerificationNotification();
 
         return $request->wantsJson()
                     ? new JsonResponse([], 202)
                     : back()->with('resent', true);
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
     }
 }
