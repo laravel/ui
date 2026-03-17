b71402bf1996e64155aafb0bdad82d07445abfcf
jnoordsij <j.noordsij@sslleiden.nl>
2026-03-17 14:26:57 +0100

    Allow PHPUnit v13
    Including rework from mock to stub in ThrottleLoginsTest

--- tests/AuthBackend/ThrottleLoginsTest.php
+++ tests/AuthBackend/ThrottleLoginsTest.php
@@ -14,7 +14,7 @@ class ThrottleLoginsTest extends TestCase
     #[DataProvider('emailProvider')]
     public function it_can_generate_throttle_key(string $email, string $expectedEmail): void
     {
-        $throttle = $this->createMock(ThrottlesLogins::class);
+        $throttle = $this->createStub(ThrottlesLogins::class);
         $throttle->method('username')->willReturn('email');
         $reflection = new \ReflectionClass($throttle);
         $method = $reflection->getMethod('throttleKey');
